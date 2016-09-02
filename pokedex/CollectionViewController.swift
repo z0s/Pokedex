926e867f8efafebe88376a007ef6f8c590a37ac3
Zeus <masgar001@gmail.com>
2016-09-02 16:09:46 -0700

    feat: Update Collection View Controller

--- pokedex/CollectionViewController.swift
+++ pokedex/CollectionViewController.swift
@@ -10,65 +10,66 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class CollectionViewController: UIViewController {
     
     
     @IBOutlet weak var collectionView: UICollectionView!
-
+    var pokemonArray = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
         title = "Pokedex"
-
+        gradientLayer()
+    
         // Register cell classes
         collectionView.delegate = self
         collectionView.dataSource = self
         self.collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        pokemonArray = PokemonDataProvider.fetchPokemon()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func gradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        gradient.colors = [red.aRed.CGColor as CGColorRef, red.bRed.CGColor as CGColorRef]
+        gradient.locations = [0.0, 1.0]
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
     // MARK: UICollectionViewDataSource
-
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 1
-    }
-
+extension CollectionViewController: UICollectionViewDataSource {
+    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CollectionViewCell
-    
+        
+        let pokemon = pokemonArray[indexPath.row]
+        cell.pokemon = pokemon
+        
         // Configure the cell
-    
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return pokemonArray.count
+    }
+    
+    
+}
+//    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+// MARK: UICollectionViewDelegate
+extension CollectionViewController: UICollectionViewDelegate {
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailController: ImageDetailViewController = storyboard.instantiateViewControllerWithIdentifier("ImageDetail") as! ImageDetailViewController
+        self.navigationController?.pushViewController(detailController, animated: true)
+    }
+}
 
  
 
-}
