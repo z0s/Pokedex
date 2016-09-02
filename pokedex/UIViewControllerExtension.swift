7944cd62ef635ab97bae44fedd823d5ddc17be69
Zeus <masgar001@gmail.com>
2016-09-02 16:12:12 -0700

    feat: Update UIView Extensions

--- pokedex/UIViewControllerExtension.swift
+++ pokedex/UIViewControllerExtension.swift
@@ -9,16 +9,67 @@
 import UIKit
 
 extension UIViewController {
-    class func gradientLayer() {
-        let gradient = CAGradientLayer()
-        gradient.frame = self.view.bounds
-        gradient.colors = [red.aRed.CGColor as CGColorRef, red.bRed.CGColor as CGColorRef]
-        gradient.locations = [0.0, 1.0]
-        self.view.layer.insertSublayer(gradient, atIndex: 0)
-    }
     
     struct red {
         static let aRed = UIColor(red:1.0, green:0.0, blue:0.0, alpha:1.00)
         static let bRed = UIColor(red: 0.6392, green: 0, blue: 0.0078, alpha: 1.0)
     }
+    
+
+    func presentAlert(error: NSError) {
+        presentAlert("Error", message: error.localizedDescription, actionTitle: "OK", actionHandler: nil)
+    }
+    
+    func presentAlert(title: String, message: String, actionTitle: String, actionHandler: ((UIAlertAction) -> Void)?) {
+        
+        let alertControllerStyle = UIAlertControllerStyle.Alert
+        let alertView = UIAlertController(title: title, message: message, preferredStyle: alertControllerStyle)
+        
+        let alertActionStyle = UIAlertActionStyle.Default
+        let alertAction = UIAlertAction(title: actionTitle, style: alertActionStyle, handler: actionHandler)
+        
+        alertView.addAction(alertAction)
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.presentViewController(alertView, animated: true, completion: nil)
+        })
+    }
+    
+    func presentAlert(title: String, message: String, actionTitle: String) {
+        
+        let alertControllerStyle = UIAlertControllerStyle.Alert
+        let alertView = UIAlertController(title: title, message: message, preferredStyle: alertControllerStyle)
+        
+        let alertActionStyle = UIAlertActionStyle.Default
+        let alertActionOK = UIAlertAction(title: actionTitle, style: alertActionStyle, handler: nil)
+        
+        alertView.addAction(alertActionOK)
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.presentViewController(alertView, animated: true, completion: nil)
+        })
+    }
+    
+    func showSpinner() -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+        dispatch_async(dispatch_get_main_queue(), {
+            spinner.center = self.view.center
+            spinner.color = UIColor.orangeColor()
+            self.view.addSubview(spinner)
+            spinner.startAnimating()
+        })
+        
+        return spinner
+    }
+    
+
 }
+extension UIActivityIndicatorView {
+    func hide() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.stopAnimating()
+            self.removeFromSuperview()
+        })
+    }
+}
+
