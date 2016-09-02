ef55a908627e59a30a5484e8cb6cde4dbbed2e53
Zeus <masgar001@gmail.com>
2016-09-02 16:12:38 -0700

    feat: Update Pokedex Data Provider

--- pokedex/PokemonDataProvider.swift
+++ pokedex/PokemonDataProvider.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2016 z0s. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import CoreData
 
 class PokemonDataProvider: NSObject {
-
+    static let stack = (UIApplication.sharedApplication().delegate as! AppDelegate).stack
+    
+    class func fetchPokemon() -> [Pokemon] {
+        let fetchRequest = NSFetchRequest(entityName: Pokemon.entityName())
+        let idSortDescriptor = NSSortDescriptor(key: PokeAPI.PokeResponseKeys.ID, ascending: true)
+        fetchRequest.sortDescriptors = [idSortDescriptor]
+        
+        do {
+            if let pokemonArray = try stack.context.executeFetchRequest(fetchRequest) as? [Pokemon] {
+                return pokemonArray
+            }
+        } catch {
+            
+        }
+        
+        return []
+    }
+    
+    class func fetchPokemonForID(id: Int) -> Pokemon? {
+        let fetchRequest = NSFetchRequest(entityName: Pokemon.entityName())
+        fetchRequest.predicate = NSPredicate(format: "id == %d", id)
+        
+        do {
+            if let pokemonArray = try stack.context.executeFetchRequest(fetchRequest) as? [Pokemon] {
+                return pokemonArray.first
+            }
+        } catch {
+            
+        }
+        
+        return nil
+    }
 }
