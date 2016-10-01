f822d642a30976a8d450a76facd4f12ae15d2f9c
Zeus <masgar001@gmail.com>
2016-10-01 10:08:51 -0700

    fix: Update to Swift 3.0

--- pokedex/CoreDataStack.swift
+++ pokedex/CoreDataStack.swift
@@ -11,24 +11,24 @@ import CoreData
 struct CoreDataStack {
     
     // MARK:  - Properties
-    private let model : NSManagedObjectModel
-    private let coordinator : NSPersistentStoreCoordinator
-    private let modelURL : NSURL
-    private let dbURL : NSURL
+    fileprivate let model : NSManagedObjectModel
+    fileprivate let coordinator : NSPersistentStoreCoordinator
+    fileprivate let modelURL : URL
+    fileprivate let dbURL : URL
     let context : NSManagedObjectContext
     
     // MARK:  - Initializers
     init?(modelName: String){
         
         // Assumes the model is in the main bundle
-        guard let modelURL = NSBundle.mainBundle().URLForResource(modelName, withExtension: "momd") else {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
             print("Unable to find \(modelName)in the main bundle")
             return nil}
         
         self.modelURL = modelURL
         
         // Try to create the model from the URL
-        guard let model = NSManagedObjectModel(contentsOfURL: modelURL) else{
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else{
             print("unable to create a model from \(modelURL)")
             return nil
         }
@@ -40,19 +40,19 @@ struct CoreDataStack {
         coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
         
         // create a context and add connect it to the coordinator
-        context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         context.persistentStoreCoordinator = coordinator
         context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         
         // Add a SQLite store located in the documents folder
-        let fm = NSFileManager.defaultManager()
+        let fm = FileManager.default
         
-        guard let  docUrl = fm.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first else{
+        guard let  docUrl = fm.urls(for: .documentDirectory, in: .userDomainMask).first else{
             print("Unable to reach the documents folder")
             return nil
         }
         
-        self.dbURL = docUrl.URLByAppendingPathComponent("model.sqlite")
+        self.dbURL = docUrl.appendingPathComponent("model.sqlite")
 
 
         let options = [NSMigratePersistentStoresAutomaticallyOption: true,
@@ -67,12 +67,12 @@ struct CoreDataStack {
     }
     
     // MARK:  - Utils
-    func addStoreCoordinator(storeType: String,
+    func addStoreCoordinator(_ storeType: String,
                              configuration: String?,
-                             storeURL: NSURL,
-                             options : [NSObject : AnyObject]?) throws{
+                             storeURL: URL,
+                             options : [AnyHashable: Any]?) throws{
         
-        try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: dbURL, options: options)
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL, options: options)
         
     }
 }
@@ -84,7 +84,7 @@ extension CoreDataStack  {
     func dropAllData() throws{
         // delete all the objects in the db. This won't delete the files, it will
         // just leave empty tables.
-        try coordinator.destroyPersistentStoreAtURL(dbURL, withType:NSSQLiteStoreType , options: nil)
+        try coordinator.destroyPersistentStore(at: dbURL, ofType:NSSQLiteStoreType , options: nil)
         
         try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
 
