8df47a7311a02bf5fa81853510ec3b6ae17fdcf4
Zeus <masgar001@gmail.com>
2016-10-01 10:06:38 -0700

    feat: Update to include tableview and web view.

--- pokedex/ImageDetailViewController.swift
+++ pokedex/ImageDetailViewController.swift
@@ -10,37 +10,52 @@ import UIKit
 
 class ImageDetailViewController: UIViewController {
     
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var webView: UIWebView!
     
-  
-    @IBAction func infoButton(sender: UIButton) {
-        presentAlert("\(pokemon.name.capitalizedString)", message: "Pokedex number: \(pokemon.id)", actionTitle: "Return")
+    let pokemonType = ["Type"]
+    let pokemonDescription = ["Description"]
+    
+    @IBAction func infoButton(_ sender: UIButton) {
+        presentAlert("\(pokemon.name.capitalized)", message: "Pokedex number: \(pokemon.id)", actionTitle: "Thanks!")
     }
     var pokemon: Pokemon! {
         didSet {
-            PokeAPI.requestPokemonDescriptionForID(pokemon.id.unsignedIntegerValue)
+            PokeAPI.requestPokemonDescriptionForID(pokemon.id.uintValue)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        title = "\(pokemon.name)"
+      
+        let url = URL(string: "http://www.pokemon.com/us/pokedex/\(pokemon.id)")
+        let request = URLRequest(url: url!)
+        webView.loadRequest(request)
+     
+        tableView.dataSource = self
+        tableView.isUserInteractionEnabled = false
         
-        descriptionLabel.text = pokemon.descriptionString
+        title = "\(pokemon.name)"
+        descriptionLabel.textColor = UIColor.clear
+        typeLabel.textColor = UIColor.black
+        
         for type in pokemon.types {
             if typeLabel.text == "" {
-                typeLabel.text = type.capitalizedString
+                typeLabel.text = type.capitalized
             } else {
                 if let typeString = typeLabel.text {
-                typeLabel.text = typeString + ", " + type.capitalizedString
+                    typeLabel.text = typeString + ", " + type.capitalized
                 }
             }
         }
         
-        gradientLayer()
+    
+        tableView.backgroundColor = UIColor(white: 0.0, alpha: 0.0)
+        
         
         if let image = UIImage(named: "\(pokemon.id)") {
             imageView.image = image
@@ -49,8 +64,8 @@ class ImageDetailViewController: UIViewController {
         
         
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(descriptionDownloaded), name: "PokemonDescriptionDidFinishDownloading", object: nil)
-
+        NotificationCenter.default.addObserver(self, selector: #selector(descriptionDownloaded), name: NSNotification.Name(rawValue: "PokemonDescriptionDidFinishDownloading"), object: nil)
+        
         
     }
     
@@ -58,13 +73,35 @@ class ImageDetailViewController: UIViewController {
         descriptionLabel.text = pokemon.descriptionString
     }
     
-    private func gradientLayer() {
-        let gradient = CAGradientLayer()
-        gradient.frame = self.view.bounds
-        gradient.colors = [red.aRed.CGColor as CGColorRef, red.bRed.CGColor as CGColorRef]
-        gradient.locations = [0.0, 1.0]
-        self.view.layer.insertSublayer(gradient, atIndex: 0)
-    }
-
 }
 
+extension ImageDetailViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let nougat = 1
+        
+        return nougat
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return pokemonDescription[0]
+        } else {
+            return pokemonType[0]
+        }
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        if indexPath.section == 0 {
+            cell.textLabel?.text = pokemon.descriptionString
+            if (cell.textLabel?.text?.isEmpty)! {
+                cell.textLabel?.text = "The description is not available."
+            }
+        }
+        
+        return cell
+    }
+    
+}
