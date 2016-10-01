9333f30bf6270c5ce616e70965fd950b73696b42
Zeus <masgar001@gmail.com>
2016-10-01 10:08:39 -0700

    feat: Update to Swift 3.0

--- pokedex/CollectionViewController.swift
+++ pokedex/CollectionViewController.swift
@@ -32,15 +32,15 @@ class CollectionViewController: UIViewController {
         // Register cell classes
         collectionView.delegate = self
         collectionView.dataSource = self
-        self.collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(CollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
         pokemonArray = PokemonDataProvider.fetchPokemon()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(downloadError), name: "PokemonDownloadError", object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(dataUpdated), name: NSManagedObjectContextObjectsDidChangeNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(downloadFinished), name: "PokemonDidFinishDownloading", object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(downloadError), name: NSNotification.Name(rawValue: "PokemonDownloadError"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(dataUpdated), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(downloadFinished), name: NSNotification.Name(rawValue: "PokemonDidFinishDownloading"), object: nil)
 
-        refreshControl.tintColor = UIColor.grayColor()
-        refreshControl.addTarget(self, action: #selector(fetchMorePokemon), forControlEvents: .ValueChanged)
+        refreshControl.tintColor = UIColor.gray
+        refreshControl.addTarget(self, action: #selector(fetchMorePokemon), for: .valueChanged)
         
         self.collectionView.addSubview(refreshControl)
         self.collectionView.alwaysBounceVertical = true
@@ -55,12 +55,12 @@ class CollectionViewController: UIViewController {
         PokeAPI.fetchNext15Pokemon()
     }
     
-    private func gradientLayer() {
+    fileprivate func gradientLayer() {
         let gradient = CAGradientLayer()
         gradient.frame = self.view.bounds
-        gradient.colors = [red.aRed.CGColor as CGColorRef, red.bRed.CGColor as CGColorRef]
+        gradient.colors = [red.aRed.cgColor as CGColor, red.bRed.cgColor as CGColor]
         gradient.locations = [0.0, 1.0]
-        self.view.layer.insertSublayer(gradient, atIndex: 0)
+        self.view.layer.insertSublayer(gradient, at: 0)
     }
     
     func downloadError() {
@@ -76,16 +76,16 @@ class CollectionViewController: UIViewController {
     // MARK: UICollectionViewDataSource
 extension CollectionViewController: UICollectionViewDataSource {
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
         
-        let pokemon = pokemonArray[indexPath.row]
+        let pokemon = pokemonArray[(indexPath as NSIndexPath).row]
         cell.pokemon = pokemon
         // Configure the cell
         return cell
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return pokemonArray.count
     }
@@ -95,18 +95,20 @@ extension CollectionViewController: UICollectionViewDataSource {
 
 // MARK: UICollectionViewDelegate
 extension CollectionViewController: UICollectionViewDelegate {
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let detailController: ImageDetailViewController = storyboard.instantiateViewControllerWithIdentifier("ImageDetail") as! ImageDetailViewController
         
-        let pokemon = pokemonArray[indexPath.row]
+        
+        let detailController: ImageDetailViewController = storyboard.instantiateViewController(withIdentifier: "ImageDetail") as! ImageDetailViewController
+        
+        let pokemon = pokemonArray[(indexPath as NSIndexPath).row]
         detailController.pokemon = pokemon
         self.navigationController?.pushViewController(detailController, animated: true)
     }
 }
 
 extension CollectionViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if (scrollView.contentOffset.y == (scrollView.contentSize.height - scrollView.frame.size.height)) {
             //reach bottom
             PokeAPI.fetchNext15Pokemon()
